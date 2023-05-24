923976c8c786ee504b1720471287792a30c44cc2
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-24 14:58:35 -0500

    updated to filter for recipient Id

--- scripts/feed/MessageList.js
+++ scripts/feed/MessageList.js
@@ -2,24 +2,30 @@
 import { fetchMessages, fetchUsers } from './data/TransientState.js';
 
 // declare and export MessageList function that generates an HTML string representing a list of messages
-export function MessageList() {
-  const messages = fetchMessages();
-  const users = fetchUsers();
-
-  const htmlStrings = messages.map(function (message) {
-    const { userId, text } = message;
-
-    const htmlString = `
-      <div class="message">
-        <span class="user">${users[userId]}</span>
-        <p class="message-text">${text}</p>
-      </div>
-    `;
-
-    return htmlString;
-  });
-
-  const resultString = htmlStrings.join('');
-
-  return resultString;
-}
+export function MessageList(recipientId) {
+    const messages = fetchMessages();
+    const users = fetchUsers();
+  
+    const htmlStrings = messages
+      .filter(function (message) {
+        // Filter messages based on the recipientId
+        return message.recipientId === recipientId;
+      })
+      .map(function (message) {
+        const { userId, text } = message;
+  
+        const htmlString = `
+          <div class="message">
+            <span class="user">${users[userId]}</span>
+            <p class="message-text">${text}</p>
+          </div>
+        `;
+  
+        return htmlString;
+      });
+  
+    const resultString = htmlStrings.join('');
+  
+    return resultString;
+  }
+  
