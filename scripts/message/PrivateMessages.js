66f171dfd1ae6a5d9809dd1629a103c11f8dc47f
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-24 15:15:26 -0500

    deleted feed/MessageList and put all code for private message list and reply button into message/PrivateMessages

--- scripts/message/PrivateMessages.js
+++ scripts/message/PrivateMessages.js
@@ -3,9 +3,39 @@ generates the HTML markup for the private message list page
 and handling reply button
 */
 
-// import NavBar from ./nav/NavBar.js
 // import MessageList from ./feed/MessageList.js
-// reply button sends to create message page
-// attach click event listener for reply button
-// declare PrivateMessageList function
-// export PrivateMessageList function
+
+    // imports getMessages and getUsers functions from ./data/TransientState.js
+import { fetchMessages, fetchUsers } from './data/TransientState.js';
+
+// declare and export MessageList function that generates an HTML string representing a list of messages
+export function MessageList(recipientId) {
+    const messages = fetchMessages();
+    const users = fetchUsers();
+  
+    const htmlStrings = messages
+      .filter(function (message) {
+        // Filter messages based on the recipientId
+        return message.recipientId === recipientId;
+      })
+      .map(function (message) {
+        const { userId, text } = message;
+  
+        const htmlString = `
+          <div class="message">
+            <span class="user">${users[userId]}</span>
+            <p class="message-text">${text}</p>
+            <button class="reply-button">REPLY</button>
+          </div>
+        `;
+  
+        return htmlString;
+      });
+  
+    const resultString = htmlStrings.join('');
+  
+    return resultString;
+  }  
+  
+  // attach click event listener for reply button
+    // reply button sends to create message page
