0809529da56c85cb7882cd0d0883b1679bde24d8
Ryan Phillips <ryanmphill@gmail.com>
2023-05-24 16:11:34 -0500

    Added function to create HTML string for Login page view

--- scripts/auth/Login.js
+++ scripts/auth/Login.js
@@ -10,3 +10,28 @@ it with a list of users, and stores the user's id if a match is found
 // declares LoginForm function
 // attach "click" event listener for Login button
 // exports LoginForm function
+
+export const LoginForm = () => {
+
+    let htmlString = `
+        <div class="loginHeader"><h1>Login Form</h1></div>  
+        <form id="loginForm">  
+            <div class="Login">   
+                <div class="Login__row Login__username">
+                    <label class="Login--label">Username : </label>   
+                    <input id="username" class="Login--input" type="text" placeholder="Enter Username" name="username" required>
+                </div>  
+                <div class="Login__row Login__password">
+                    <label class="Login--label">Password : </label>
+                    <input id="password" class="Login--input" type="password" placeholder="Enter Password" name="password" required>
+                </div>   
+                  
+                <div class="Login__row Login__buttonRow">
+                    <button id="loginButton" class="Login--btn"  type="button">Login</button>
+                </div>   
+      
+            </div>   
+        </form>
+    `
+    return htmlString
+}
