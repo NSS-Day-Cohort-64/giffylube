9edc6bf17c482832a63e22c134cb60cf86a79c77
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-24 15:42:05 -0500

    updated code with Chesney's changes

--- scripts/message/PrivateMessages.js
+++ scripts/message/PrivateMessages.js
@@ -6,36 +6,41 @@ and handling reply button
 // import MessageList from ./feed/MessageList.js
 
     // imports getMessages and getUsers functions from ./data/TransientState.js
-import { fetchMessages, fetchUsers } from './data/TransientState.js';
+import { fetchMessages, fetchUsers, getCurrentUser } from './data/TransientState.js';
 
 // declare and export MessageList function that generates an HTML string representing a list of messages
-export function MessageList(recipientId) {
+// make sure you are checking that the message.recipientId===currentUser.userId
+// do we need the return in the .filter function?
+// make a new variable for the new array of filtered messages and then iterate over that array with .map
+
+export function MessageList(currentUser) {
     const messages = fetchMessages();
     const users = fetchUsers();
   
-    const htmlStrings = messages
-      .filter(function (message) {
-        // Filter messages based on the recipientId
-        return message.recipientId === recipientId;
-      })
-      .map(function (message) {
-        const { userId, text } = message;
+    const filteredMessages = messages.filter(function (message) {
+      // Filter messages based on the recipientId matching the currentUser's userId
+      return message.recipientId === currentUser.userId;
+    });
   
-        const htmlString = `
-          <div class="message">
-            <span class="user">${users[userId]}</span>
-            <p class="message-text">${text}</p>
-            <button class="reply-button">REPLY</button>
-          </div>
-        `;
+    const htmlStrings = filteredMessages.map(function (message) {
+      const { userId, text } = message;
   
-        return htmlString;
-      });
+      const htmlString = `
+        <div class="message">
+          <span class="user">${users[userId]}</span>
+          <p class="message-text">${text}</p>
+          <button class="reply-button">REPLY</button>
+        </div>
+      `;
+  
+      return htmlString;
+    });
   
     const resultString = htmlStrings.join('');
   
     return resultString;
-  }  
+  }
+  
   
   // attach click event listener for reply button
     // reply button sends to create message page
