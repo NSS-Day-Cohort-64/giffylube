3b9b23ebadfd0d7de3d0fd10bda44122c5850113
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-24 14:32:21 -0500

    MessageList HTML starter code

--- scripts/feed/MessageList.js
+++ scripts/feed/MessageList.js
@@ -1,7 +1,25 @@
-/*
-generates an HTML string representing a list of messages
-*/
-
 // imports getMessages and getUsers functions from ./data/TransientState.js
-// declare MessageList function
-// export MessageList function
+import { getMessages, getUsers } from './data/TransientState.js';
+
+// declare and export MessageList function that generates an HTML string representing a list of messages
+export function MessageList() {
+  const messages = getMessages();
+  const users = getUsers();
+
+  const htmlStrings = messages.map(function (message) {
+    const { userId, text } = message;
+
+    const htmlString = `
+      <div class="message">
+        <span class="user">${users[userId]}</span>
+        <p class="message-text">${text}</p>
+      </div>
+    `;
+
+    return htmlString;
+  });
+
+  const resultString = htmlStrings.join('');
+
+  return resultString;
+}
