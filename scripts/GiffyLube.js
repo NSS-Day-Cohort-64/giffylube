d43fb946e8f7d4ba62b3228ec84380bf5ff12314
Chesney Hardin <chesney.hardin@gmail.com>
2023-05-25 13:34:02 -0500

    workin on giffylube HTML

--- scripts/GiffyLube.js
+++ scripts/GiffyLube.js
@@ -1,4 +1,6 @@
 // import NavBar from ./nav/NavBar.js
+import { getView } from "./data/TransientState.js"
+import { MessageList } from "./message/PrivateMessages.js"
 import { NavBarHTML } from "./nav/NavBar.js"
 // import PostEntry from ./feed/PostEntry.js
 // import PostList from ./feed/PostList.js
@@ -8,29 +10,43 @@ import { NavBarHTML } from "./nav/NavBar.js"
 // import getView function from ./data/TransientState.js
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/* 
-    Define and export GiffyLube function that generates all the html for main feed page
-    Don't forget to await functions with embedded fetch calls
-
-    Create variable for htmlString (or another name) = NavBar //////////////
-
-    check these conditions of the transientState to determine which page to generate (only one or none should be true)
-        1. If transientState.view===viewMessages, then invoke function that generates all the HTML for the inbox page and add it to the htmlString
-        2. If transientState.view===createMessage, then invoke function that generates all the HTML for the create message page and add it to the htmlString
-        3. If transientState.view===postGif, then invoke function that generates all the HTML for the new post page and add it to the htmlString
-        4. If transientState.view===defaultView, generate the HTML for the main feed page and add it to the htmlString
-*/
+//    Define and export GiffyLube function that generates all the html for main feed page
 export const GiffyLube = async () => {
     const navBar = await NavBarHTML()
+    const messages = await MessageList()
+    const view = getView()
     
+    //Create variable for htmlList = NavBar 
     let htmlList = `${navBar}`
+
+    //check these conditions of the transientState to determine which page to generate 
+        //1. If transientState.view===viewMessages, then invoke function that generates all the HTML for the inbox page and add it to the htmlString
+        if(view === "viewMessages") {
+            htmlList += `${}`
+        }
+
+        //2. If transientState.view===createMessage, then invoke function that generates all the HTML for the create message page and add it to the htmlString
+        if(view === "createMessage") {
+            htmlList += `${}`
+        }
+
+        //3. If transientState.view===postGif, then invoke function that generates all the HTML for the new post page and add it to the htmlString
+        if(view === "postGif") {
+            htmlList += `${}`
+        }
+        
+        //4. If transientState.view===defaultView, generate the HTML for the main feed page and add it to the htmlString
+        if(view === "defaultView") {
+            htmlList += `${}`
+        }
+        
+
     htmlList += `
     <h1>This will be the main page</h1>
     `
     return htmlList
 }
-// export GiffyLube function
+
 
 // click event listener for "have a gif to post?" to change transient state (outside any function)
 
