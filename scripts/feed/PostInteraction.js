adb487a7bac16f252043072396334c4945a64853
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-25 14:08:04 -0500

    PostInteraction module code and updated PostList module code

--- scripts/feed/PostInteraction.js
+++ scripts/feed/PostInteraction.js
@@ -3,8 +3,32 @@ handles the rendering and interaction of a post element
 */
 
 // import deletePost, favoritePost from ./data/TransientState.js
+import { deletePost, favoritePost } from './data/TransientState.js';
+
 // attach 2 click event listeners -- both outside any function:
     // Delete button
     // Favorite button
-// declare PostInteraction function
-// export PostInteraction function to PostList
+document.addEventListener('click', handleDeleteButtonClick);
+document.addEventListener('click', handleFavoriteButtonClick);
+
+// declare and export PostInteraction function
+export const PostInteraction = (postId) => {
+  const handleDeleteButtonClick = (event) => {
+    if (event.target.classList.contains('delete-button')) {
+      deletePost(postId);
+    }
+  };
+
+  const handleFavoriteButtonClick = (event) => {
+    if (event.target.classList.contains('favorite-button')) {
+      favoritePost(postId);
+    }
+  };
+
+  return `
+    <div class="post-interaction">
+      <button class="delete-button">DELETE</button>
+      <button class="favorite-button">FAVORITE</button>
+    </div>
+  `;
+};
