1e0a49377249f1d2102e3b30a8ebed7f4d667399
Chesney Hardin <chesney.hardin@gmail.com>
2023-05-25 10:50:24 -0500

    changes

--- scripts/nav/NavBar.js
+++ scripts/nav/NavBar.js
@@ -8,20 +8,14 @@ import { fetchMessages, getCurrentUser } from "../data/TransientState.js"
 // import clearFilters (if Footer happens), getMessages, setView, displayUsername from ./data/TransientState.js
 
 // attach 4 click event listeners outside of any functions for
-// Notifications - reset transient state view to viewMessages
+// Notifications - set transient state view to viewMessages
+
 // show how many messages view getMessages
 // Create Message pen icon - change view to createMessage
 // Logout - reset current user to 0
 // Logo - reset transient state view to defaultView
 
 // declare and export NavBar function
-// return HTML string including username - displayUsername
-// Logout button
-// Notifications icon
-// Logo icon
-// Create Message icon
-// Site Title
-
 
 export const NavBarHTML = async () => {
     const currentUser = getCurrentUser()
