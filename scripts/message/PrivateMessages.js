fd61c32228d9b6d55d21ebe38c36c4d87955448f
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-26 10:29:35 -0500

    PrivateMessages event listener for reply button

--- scripts/message/PrivateMessages.js
+++ scripts/message/PrivateMessages.js
@@ -1,17 +1,4 @@
-/*
-generates the HTML markup for the private message list page
-and handling reply button
-*/
-
-// import MessageList from ./feed/MessageList.js
-
-    // imports getMessages and getUsers functions from ./data/TransientState.js
 import { fetchMessages, fetchUsers, getCurrentUser } from '../data/TransientState.js';
-
-// declare and export MessageList function that generates an HTML string representing a list of messages
-// make sure you are checking that the message.recipientId===currentUser.userId
-// do we need the return in the .filter function?
-// make a new variable for the new array of filtered messages and then iterate over that array with .map
 
 export const MessageList = async () => {
   const messages = await fetchMessages();
@@ -19,16 +6,15 @@ export const MessageList = async () => {
   const currentUser = getCurrentUser();
 
   const filteredMessages = messages.filter((message) => {
-    // Filter messages based on the recipientId matching the currentUser's userId
     return message.recipientId === currentUser.userId;
   });
-  
+
   const htmlStrings = filteredMessages.map((message) => {
     const { userId, text } = message;
-  
+
     const sender = users.find((user) => user.id === userId);
     const senderName = sender ? sender.name : 'Unknown User';
-  
+
     const htmlString = `
       <div class="message">
         <span class="user">From: ${senderName}</span>
@@ -36,16 +22,23 @@ export const MessageList = async () => {
         <button class="reply-button">REPLY</button>
       </div>
     `;
-  
+
     return htmlString;
   });
-  
-  const resultString = htmlStrings.join('');
-  
-  return resultString;
 
-}
-  
-  
-  // attach click event listener for reply button
-    // reply button sends to create message page
+  const resultString = htmlStrings.join('');
+
+  const container = document.createElement('div');
+  container.innerHTML = resultString;
+
+  const replyButtons = container.querySelectorAll('.reply-button');
+  replyButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      setView('createMessage');
+      const customEvent = new CustomEvent('stateChanged');
+      document.dispatchEvent(customEvent);
+    });
+  });
+
+  return container;
+};
