7cc295b2d571d581e7598270f9cfd6d8a6ef54fe
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-26 09:25:36 -0500

    new PostInteraction, new TransientState, new database

--- scripts/feed/PostInteraction.js
+++ scripts/feed/PostInteraction.js
@@ -3,28 +3,28 @@ handles the rendering and interaction of a post element
 */
 
 // import deletePost, favoritePost from ./data/TransientState.js
-import { deletePost, favoritePost } from '../data/TransientState.js';
+// import { deletePost, favoritePost } from '../data/TransientState.js';
 
 // attach 2 click event listeners -- both outside any function:
     // Delete button
     // Favorite button
-    const handleDeleteButtonClick = (postId) => {
-        deletePost(postId);
-      };
+    // const handleDeleteButtonClick = (postId) => {
+    //     deletePost(postId);
+    //   };
       
-      const handleFavoriteButtonClick = (postId) => {
-        favoritePost(postId);
-      };
+    //   const handleFavoriteButtonClick = (postId) => {
+    //     favoritePost(postId);
+    //   };
       
-      document.addEventListener('click', (event) => {
-        if (event.target.classList.contains('delete-button')) {
-          handleDeleteButtonClick(event.target.dataset.postId);
-        }
+    //   document.addEventListener('click', (event) => {
+    //     if (event.target.classList.contains('delete-button')) {
+    //       handleDeleteButtonClick(event.target.dataset.postId);
+    //     }
       
-        if (event.target.classList.contains('favorite-button')) {
-          handleFavoriteButtonClick(event.target.dataset.postId);
-        }
-      });
+    //     if (event.target.classList.contains('favorite-button')) {
+    //       handleFavoriteButtonClick(event.target.dataset.postId);
+    //     }
+    //   });
 
 // declare and export PostInteraction function
 export const PostInteraction = (postId) => {
